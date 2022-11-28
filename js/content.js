c536842671e6bab5e559939cbe4dd9c33f9dab69
Sean Corbett <stcorbett@gmail.com>
2022-11-27 18:40:37 -0800

    rendering filtered experiences

--- js/content.js
+++ js/content.js
@@ -1,11 +1,11 @@
 export default class {
   static experiences = [
     {
-      name: "tech",
-      title: "abc",
-      role: "123",
+      name: "aba-session",
+      title: "ABA Session",
+      role: "Founder and CEO, 2020-2022 &nbsp; Advisor 2022-present",
       content: [
-        "def",
+        "Transforming a healthcare niche with software tools. Co-designed with users, developed the product, hired and managed engineering and operational teams. Mentored recent CS graduates on the engineering team, leading them to become proficient software engineers.",
         "jkl"
       ]
     },
@@ -18,8 +18,14 @@ export default class {
       name: "tech",
       title: "Technology",
       urlParam: "tech",
-      filter: ["a", "b", "c"]
-    }
+      content: ["aba-session", "b", "c"]
+    },
+    {
+      name: "real_estate",
+      title: "Real Estate",
+      urlParam: "re",
+      content: ["real_estate", "b", "c"]
+    },
   ]
 
 }
--- js/experience_controller.js
+++ js/experience_controller.js
@@ -2,12 +2,49 @@ import { Controller } from "../node_modules/@hotwired/stimulus/dist/stimulus.js"
 import Content from "./content.js"
 
 export default class extends Controller {
+  static targets = ["itemTemplate"]
+
   connect () {
     this.key = window.location.search.split("?")[1]
-    // console.log("versions: ", this.constructor.versions)
-    console.log("content: ", Content.experiences[0])
-    console.log("content: ", Content.versions[0])
+
+    this.render()
   }
 
+  filter() {
+    if(this.key == 'all') {
+      return this.key
+    }
 
+    return Content.versions.find(version => version.urlParam == this.key)
+  }
+
+  content() {
+    if(this.filter() == 'all') {
+      return Content.experiences
+    }
+
+    return Content.experiences.filter(content => this.filter().content.includes(content.name))
+  }
+
+  render() {
+    if(this.content().length) this.renderExperiences()
+    else this.renderExperiencesPicker()
+  }
+
+  renderExperiences() {
+    this.content().forEach(experience => {
+      let item = document.createElement("div")
+      item.innerHTML = this.itemTemplateTarget.innerHTML
+
+      item.querySelector('[data-content="title"]').innerHTML = experience.title
+      item.querySelector('[data-content="role"]').innerHTML = experience.role
+      item.querySelector('[data-content="content-li"]').innerHTML = experience.content[0]
+
+      this.itemTemplateTarget.after(item)
+    });
+  }
+
+  renderExperiencesPicker() {
+    console.log("rendering picker")
+  }
 }
