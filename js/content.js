f8d8884b956626aa885d1c33cedfa7e219a596a3
Sean Corbett <stcorbett@gmail.com>
2022-12-03 17:58:32 -0600

    update experiences content

--- js/content.js
+++ js/content.js
@@ -5,13 +5,65 @@ export default class {
       title: "ABA Session",
       role: "Founder and CEO, 2020-2022 &nbsp; Advisor 2022-present",
       content: [
-        "Transforming a healthcare niche with software tools. Co-designed with users, developed the product, hired and managed engineering and operational teams. Mentored recent CS graduates on the engineering team, leading them to become proficient software engineers.",
-        "jkl",
-        "eef"
+        "Transforming a healthcare niche with software tools.",
+        "Co-designed with users, developed the product, hired and managed engineering and operational teams.",
+        "Mentored recent CS graduates on the engineering team, leading them to become proficient software engineers."
       ]
     },
 
-    // another
+    {
+      name: "provi",
+      title: "Provi",
+      role: "Engineering Leader, 2019",
+      content: [
+        "Creating a company culture of individual ownership of the business's success.",
+        "Coached a team of five to communicate clearly with business owners",
+        "Shift product vision to long-term company objectives."
+      ]
+    },
+
+    {
+      name: "hire-abby",
+      title: "Hire Abby",
+      role: "Founder and CTO, 2016-19",
+      content: [
+        "Created a system that delivers candidate engagement with an 80% cost reduction in half the time as the industry standard",
+        "Sold a six-figure software licensing agreement to a Fortune-500 firm"
+      ]
+    },
+
+    {
+      name: "tmlp",
+      title: "Team, Management, and Leadership Program",
+      role: "Team Leader, 2018-19",
+      content: [
+        "Generated $78,000 in revenue in one quarter by leading a team of 23 to deliver Communication Courses using a coaching management style.",
+        "Generated $1.2m in revenue in six months as an advisor and coach to 24 North American teams.",
+        "Developed powerful leadership techniques from trust and authenticity."
+      ]
+    },
+
+    {
+      name: "fibroblast",
+      title: "Fibroblast",
+      role: "Lead Engineer, 2013-14 &nbsp; Interim CTO, 2014-15",
+      content: [
+        "Developed product offering that combined provider and payer value.",
+        "Culturally prepared the company for growth and acquisition by Cerner Corporation."
+      ]
+    },
+
+    {
+      name: "enova",
+      title: "Enova International",
+      role: "Senior Engineer, 2008-10 &nbsp; New initiatives consultant, 2011-13",
+      content: [
+        "Covered the gamut of front end, UI, and integration with internal and external services.",
+        "Improved performance of internal services for post-processing customer credit applications.",
+        "Lead the team in rates of planned and completed work."
+      ]
+    },
+
   ]
 
   static versions = [
@@ -24,13 +76,13 @@ export default class {
       name: "senior-engineer",
       title: "Senior engineer",
       urlParam: "senior-engineer",
-      content: ["aba-session", "b", "c"]
+      content: ["aba-session", "provi", "hire-abby", "tmlp", "fibroblast", "enova"]
     },
     {
       name: "engineering-manager",
       title: "Engineering manager",
       urlParam: "engineering-manager",
-      content: ["real_estate", "b", "c"]
+      content: ["aba-session", "provi", "hire-abby", "tmlp", "fibroblast", "enova"]
     },
   ]
 
