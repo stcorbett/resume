eb935de846f21a3de64c16c02f06a0b83d42e33d
Sean Corbett <stcorbett@gmail.com>
2022-12-29 19:06:26 -0600

    content, version picker

--- js/content.js
+++ js/content.js
@@ -8,6 +8,37 @@ export default class {
         "Transforming a healthcare niche with software tools.",
         "Co-designed with users, developed the product, hired and managed engineering and operational teams.",
         "Mentored recent CS graduates on the engineering team, leading them to become proficient software engineers."
+      ],
+      stories: [
+        `
+          L2IG-WM: A partner in one of my businesses was becoming disengaged, not showing up for our meetings on time, not producing results
+          on the projects she picked to work on in the business. We had a phone call where I brought up my concern that she was not
+          as engaged as she had been in the past. She opened up, shared some anxieties about her life, the kind of work we were doing
+          and how that connected with her memories from childhood. An authentic space opened up between us in our conversation,
+          she felt heard and understood, and was able to reflect on what was most important to our business at that time and
+          my understanding of how we would reach our goals. She started to believe we had a shot at achieving our goals.
+          From there she cam up with a way for her to work on our marketing presence that supported not only her current needs in life,
+          but supported our business goals. She ended up runing our YouTube channel for several months, producing 40 videos
+          that have resulted in 10's of thousands of people viewing our content and getting exposed to our brand as a helpful
+          source of information that would help them solve the kinds of problems our products are designed to solve.
+        `,
+        `
+          LeadTr&Auth: A junior engineer was not delivering on her promise to complete 6 points in our project tracker each sprint.
+          I acknowledged that I had dropped the ball in an area that was adjacent to her work (not one I had promised to deliver on
+          but an area that effected her). She was re-engaged as of that day went on an 8 week streak of producing
+          at the highest level of her career.
+        `,
+        `
+          Software4Needs: In researching the business opportunities in the ABA field, I spoke with many disgruntled people who worked in the field.
+          The each had a different set of issues they were most concerned with, parents not participating as needed, co-workers dropping the ball,
+          insurance companies not following through, supervisors expecting impossible levels of performance.
+
+          When I broke those issues down into core problems, I was able to see a trend of the work environment not supporting the work
+          that needed to be done and therefore people not being able to perform at the levels they wanted to perform.
+
+          When I summarized people's issues in this framework, they were overjoyed to hear someone understood their needs and were eager to
+          help us build the kinds of tools we've designed to support their work and the results they want to have with their clients.
+        `
       ]
     },
 
@@ -55,6 +86,41 @@ export default class {
         "Creating a company culture of individual ownership of the business's success.",
         "Coached a team of five to communicate clearly with business owners",
         "Shift product vision to long-term company objectives."
+      ],
+      stories: [
+        `
+          L2IG-WM & LeadTr&Auth: The customer success team was under pressure to improve their speed to import data sets for new markets
+          but their day-to-day productivity was slowed by multiple workarounds they were currently performing manually to have
+          the application work as customers expected. Each week the product team would get several requests to change how the admin
+          system worked, directly from the success team, so that they could improve their process. We would tackle one or two requests
+          but they were piling up faster than we were completing them and they were not high priority for us. Management was
+          not happy with the CS team's performance, and was activitely looking to replace people as the company depended on
+          the CS team improving their throughput for upcoming expansions into new markets.
+
+          I noticed each of the feature requestes were tied to a certain type of transaction from our customers.
+          Could there be an approach that would solve several problems at the same time?
+          By building relationships with the CS team and listening to how each individual got the different tasks completed,
+          we were able to co-create an additional notification in their Slack channel that gave them everything they needed to know
+          about each new issue, and gave them links to directly access filtered result sets in the admin system so they could resolve the issues
+          with just a few clicks.
+
+          The team's performance improved, the people who helped design the new message were happy with their ability to impact
+          their work, and they were able to commit to the expansion plans the company was undertaking.
+        `,
+        `
+          Software4Needs: The data import team was slow on opening new markets.
+          This hit at the core of our strategy and goals for the next 3 years.
+          At the center of their work was building geo zones to allow list different products
+          from different distributors per the geographical agreements those distributors had
+
+          By getting to know some members of the team and understanding their confusion with the
+          geo fencing math and the interfaces for it
+          I was able to design a visualization tool to show how the math worked and
+          make the admin interface link directly to what was happeneing behind the scenes
+
+          The team loved the new interface and immediately improved their performance
+          even before we implimented the feature.
+        `
       ]
     },
 
@@ -65,6 +131,46 @@ export default class {
       content: [
         "Created a system that delivers candidate engagement with an 80% cost reduction in half the time as the industry standard",
         "Sold a six-figure software licensing agreement to a Fortune-500 firm"
+      ],
+      stories: [
+        `
+          L2IG-WM: business partner was ready to pull out of our first fortune 500 sales opportunity because
+          the actual users didn't understand the product.
+
+          I worked with him to undderstand what was going on, what they were saying.
+          Turns out, they didn't understand what the product was doing
+          We designed a dashboard together that would allow them to see the internals enough
+          to understand how it workeed and use it for themselves
+
+          At the pitch the next day, the holdout users were sold about the new dashboard and we closed the deal!
+        `,
+        `
+          LeadTr&Auth: business partner was overwhelmed, not showing up to work, not productive.
+          We had a lot to do, and customers who were taking advantage of our product.
+          He had his head down in the day to day operations, didn't see that our progress
+          was held back by being overwhelmed
+
+          I was able to make space for his emotional state - which allowed him to
+          have a fresh perspective on the business and work with me to build
+          a process for the tasks that took up 80% of his work - so we could hire someone
+          to operate those processes
+
+          We hired a friend, who performed as well or better than my partner at the task
+          and my partner was freed up to go after larger contracts.
+          Resulting in our first sale to a fortune 500 business.
+        `,
+        `
+          Software4Needs: Or product was initially email based, our customers were interested
+          but had trouble seeing how it would fit into their day to day workflows
+          so they were not buying
+
+          We researched their processes and watched them perform their work.
+          I designed and built a google sheets integration that had the output
+          of our system rendering directly into connected spreadsheets
+
+          We ended up closing our first sale, and then three more in the
+          several weeks following
+        `
       ]
     },
 
@@ -76,6 +182,50 @@ export default class {
         "Generated $78,000 in revenue in one quarter by leading a team of 23 to deliver Communication Courses using a coaching management style.",
         "Generated $1.2m in revenue in six months as an advisor and coach to 24 North American teams.",
         "Developed powerful leadership techniques from trust and authenticity."
+      ],
+      stories: [
+        `
+          L2IG-WM: I started my tenure as team leader in the midst of
+          several team members being unhappy about their commitments with the project
+          People were not showing up to our phone calls, and not taking the early steps
+          to accomplish the goals they were commited to
+
+          I worked with the 6 people who had stepped into leadership positions
+          in the team, spending time with them focused on their lives, their goals,
+          their needs.
+
+          The leadership team, and the teams each of them were leading
+          started showing up to our meetings and started taking the needed steps
+          to accomplish their goals
+        `,
+        `
+          LeadTr&Auth: The team was disengaged, not bringing themselves to our shared commitments
+
+          I opened up, shared my emotions about conflicts in my life, what was working
+          and what was hard. I kept the emotional lines open between myselves
+          and the team members.
+
+          People got engaged, started sharing their own wins and fails.
+          The leadership team, and the teams each of them were leading
+          started performing at higher levels than they had in the prior year of
+          working together.
+        `,
+        `
+          Software4Needs: The team was disorganized and taking much longer than needed
+          to communicate basic information. People were working hard but not much
+          was getting done.
+
+          I listened to some of the people I didn't know as well, people from different backgrounds
+          than my own. I started to understand the trouble the spreadsheets we had created
+          to keep track of our goals and our progress were casuing, they didn't understand
+          spreadsheets, especially not complex sheets. I suggested we move our important communication
+          to a new Slack instance. We could handle all of our organizations communication
+          needs there, skipping emial and text messages
+          and keep track of goals and progress there in text format, rather than spreadsheets
+
+          People were immediately more engaged, communication overhead went down drastically
+          and we performed in sync together and accomplished our goals.
+        `
       ]
     },
 
@@ -109,6 +259,7 @@ export default class {
         "built from scratch in a weekend",
         "supported hundreds of DAUs",
         "Influenced Uber's business model through conversations with former CEO (UberX 2012)",
+        "sold into Starbucks and Argo tea - didn't close Gap",
       ]
     },
 
@@ -139,7 +290,7 @@ export default class {
       subTitle: "Mentor, 2010 class",
       content: [
         "xxx now Brilliant.org",
-        "gave perspective on how software skills can be used in the real world"
+        "Worked with Kevin Willer to give perspective on how software skills can be used in the real world"
       ]
     },
     {
@@ -164,6 +315,14 @@ export default class {
       title: "Improv Everywhere - No Pants Subway Ride",
       content: [
         "Chicago Tribune - front-page Cover photo and interviewee",
+      ]
+    },
+    {
+      name: "lakes-of-fire",
+      title: "Lakes of Fire - Regional Burn Event",
+      content: [
+        "What Where When form - distrubuted event management",
+        "Provided medical assistance and leadership in a time of crisis, prevented loss of life",
       ]
     },
   ]
