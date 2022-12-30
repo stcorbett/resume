03059ada5923221032e7bb6d0c026aa7cffe78c1
Sean Corbett <stcorbett@gmail.com>
2022-12-29 19:06:26 -0600

    content, version picker

---
+++ js/key_select_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "../node_modules/@hotwired/stimulus/dist/stimulus.js"
+import Content from "./content.js"
+import Params from "./params.js"
+
+export default class extends Controller {
+  static targets = ["select"]
+
+  connect () {
+    this.version = Content.versions.find(version => version.urlParam == Params.key)
+
+    if (this.version) {
+      this.renderSelect()
+    }
+  }
+
+  change(event) {
+    let el = event.currentTarget
+    window.location.search = `?${el.value}`
+  }
+
+  renderSelect() {
+    this.element.classList.remove('d-none')
+
+    this.selectTarget.value = this.version.urlParam
+  }
+}
