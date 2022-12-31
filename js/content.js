b350aac88e0e7696626da7b5e28c62312a89d431
Sean Corbett <stcorbett@gmail.com>
2022-12-30 18:46:14 -0600

    content updates

--- js/content.js
+++ js/content.js
@@ -5,9 +5,9 @@ export default class {
       title: "ABA Session",
       subTitle: "Founder and CEO, 2020-2022 &nbsp; Advisor 2022-present",
       content: [
-        "Transforming a healthcare niche with software tools.",
-        "Co-designed with users, developed the product, hired and managed engineering and operational teams.",
-        "Mentored recent CS graduates on the engineering team, leading them to become proficient software engineers."
+        "Delivered modern tools to hundres of users in an overlooked healthcare niche",
+        "Co-designed with users, developed the product, hired and managed engineering and operational teams",
+        "Mentored recent CS graduates on the engineering team, leading them to become proficient software engineers"
       ],
       stories: [
         `
@@ -47,11 +47,10 @@ export default class {
       title: "Crafty Machine",
       subTitle: "Principal Consultant, 2009-present",
       content: [
-        "GFS project - fast setup",
-        "GFS project - built to fit the client's needs",
-        "GFS project - configurable by admins",
-        "GFS project - delivers large queries quickly",
-        "GFS project - scales to concurrent users",
+        "Delivered dozens of custom web applications for businesses expanding their market footprints",
+        "Achieved 100% retention rate by focusing on alignment between client needs and software processes",
+        "Designed, implimented, and supported multiple applications with thousands of daily active users",
+        "Experienced the results of design paradigms across the spectrum of software engineering"
       ]
     },
 
@@ -60,21 +59,20 @@ export default class {
       title: "Mohawk St. AirBnB",
       subTitle: "Team Leader, 2017-present",
       content: [
-        "revenue numbers and 100% y/y growth",
-        "co-design the asthetics",
-        "build and manage operational team - fun, positive, personal growth",
-        "Integrate guest interaction automation - superhost",
+        "Achieved 100% y/y growth by focusing on customer needs and market dynamics",
+        "Created a fun, positive working culture by focusing on personal growth",
+        "Achieved superhost status with automated guest interaction and hand picked operational team",
       ]
     },
 
     {
       name: "newline",
       title: "Newline Financial",
-      subTitle: "xxx",
+      subTitle: "Director of Technology, 2020",
       content: [
-        "Rescued failing development project",
-        "Build relationships and scaled down team over 6 months",
-        "Supported management in ended the project"
+        "Delivered a functional prototype of what had been a failing development project",
+        "Built relationships with remote team and scaled them down over a 6 month timeline",
+        "Supported management in drawing conclusions from the project and shutting it down"
       ]
     },
 
@@ -83,9 +81,9 @@ export default class {
       title: "Provi",
       subTitle: "Engineering Leader, 2019",
       content: [
-        "Creating a company culture of individual ownership of the business's success.",
-        "Coached a team of five to communicate clearly with business owners",
-        "Shift product vision to long-term company objectives."
+        "Created a culture of individual ownership of the business's success in the product and engineering teams",
+        "Coached the five person engineering team on clearl communication with business owners",
+        "Supported engineering and product teams in creating a product vision matched to the company's long-term strategy"
       ],
       stories: [
         `
@@ -129,8 +127,9 @@ export default class {
       title: "Hire Abby",
       subTitle: "Founder and CTO, 2016-19",
       content: [
-        "Created a system that delivers candidate engagement with an 80% cost reduction in half the time as the industry standard",
-        "Sold a six-figure software licensing agreement to a Fortune-500 firm"
+        "Built a series of production pilots, tested market applications for AI driven chat technology",
+        "Created a system that delivered job candidate engagement with an 80% cost reduction in half the time as the industry standard",
+        "Sold six-figure software licensing agreements to Fortune-500"
       ],
       stories: [
         `
@@ -179,9 +178,9 @@ export default class {
       title: "Team, Management, and Leadership Program",
       subTitle: "Team Leader, 2018-19",
       content: [
-        "Generated $78,000 in revenue in one quarter by leading a team of 23 to deliver Communication Courses using a coaching management style.",
-        "Generated $1.2m in revenue in six months as an advisor and coach to 24 North American teams.",
-        "Developed powerful leadership techniques from trust and authenticity."
+        "Generated $78,000 in revenue in one quarter by leading a team of 23 to deliver Communication Courses using coaching style management",
+        "Generated $1.2m in revenue in six months as an advisor and coach to 24 North American teams",
+        "Developed powerful leadership techniques from trust and authenticity"
       ],
       stories: [
         `
@@ -234,8 +233,8 @@ export default class {
       title: "Fibroblast",
       subTitle: "Lead Engineer, 2013-14 &nbsp; Interim CTO, 2014-15",
       content: [
-        "Developed product offering that combined provider and payer value.",
-        "Culturally prepared the company for growth and acquisition by Cerner Corporation."
+        "Designed and developed product offering that combined value for health care providers and payers",
+        "Prepared the company culture for growth and later acquisition by Cerner Corporation"
       ]
     },
 
@@ -244,10 +243,10 @@ export default class {
       title: "Enova International",
       subTitle: "Senior Engineer, 2008-10 &nbsp; New initiatives consultant, 2011-13",
       content: [
-        "Covered the gamut of front end, UI, and integration with internal and external services.",
-        "Improved performance of internal services for post-processing customer credit applications.",
-        "Lead the team in rates of planned and completed work.",
-        "Scaled systems and users - numbers here"
+        "Contributed to project successes in back end engineering projects, front end, UI, and internal and external service integrations",
+        "Improved performance of internal services for post-processing customer credit applications reducing time to approval by 50%",
+        "Lead the team in rates of planned and completed work",
+        "Consistently delivered software that performed for over 100,000 daily active users"
       ]
     },
 
@@ -256,10 +255,10 @@ export default class {
       title: "Have My Shift",
       subTitle: "Founder and CTO, 2008-9 &nbsp; CEO, 2009-11",
       content: [
-        "built from scratch in a weekend",
-        "supported hundreds of DAUs",
-        "Influenced Uber's business model through conversations with former CEO (UberX 2012)",
-        "sold into Starbucks and Argo tea - didn't close Gap",
+        "Designed and implimented funcational protype in 48 hours",
+        "Uncovered additional customer needs and scaled the system up to serve thousands of users daily",
+        "Influenced the development of UberX's sharing economy services through conversations with their future CEO",
+        "Closed and delivered on business agreements local and national chains including Starbucks and Argo tea",
       ]
     },
 
@@ -267,12 +266,11 @@ export default class {
 
   static community = [
     {
-      name: "starterleague",
-      title: "Starter League (Code Academy)",
-      subTitle: "Mentor, 2011 and 2012 classes",
+      name: "lakes-of-fire",
+      title: "Lakes of Fire - Regional Burn Event",
       content: [
-        "supported new developers",
-        "gave perspective on how software skills can be used in the real world"
+        "Designed and continued to enhance What Where When - distrubuted event management application",
+        "Provided medical assistance and leadership in a time of crisis, prevented loss of life",
       ]
     },
     {
@@ -280,34 +278,41 @@ export default class {
       title: "Techstars (Excelerate Labs Chicago)",
       subTitle: "Mentor, 2010 class",
       content: [
-        "xxx now Brilliant.org",
+        "Early advisor to portfolio of companies now valued at over $1bn",
         "gave perspective on how software skills can be used in the real world"
-      ]
-    },
-    {
-      name: "1871",
-      title: "1871 Chicago - Independent Innovation hub",
-      subTitle: "Mentor, 2010 class",
-      content: [
-        "xxx now Brilliant.org",
-        "Worked with Kevin Willer to give perspective on how software skills can be used in the real world"
       ]
     },
     {
       name: "scale-well",
       title: "Scale Well - Entrepreneurial Grants",
-      subTitle: "Mentor, 2010 class",
+      subTitle: "Founder and Executive Director",
       content: [
-        "forum with keywell and XXXX",
-        "Chicago Tribune article "
+        "Interviewed for future visions of technology business in Chicago alongside Brad Keywell",
+        "Featured in the Chicago Tribune business section, cover page"
+      ]
+    },
+    {
+      name: "starterleague",
+      title: "Starter League (Code Academy)",
+      subTitle: "Mentor, 2011 and 2012 classes",
+      content: [
+        "Provided perspective on using software skills in business and the real world"
+      ]
+    },
+    {
+      name: "1871",
+      title: "1871 Chicago - Independent Innovation hub",
+      subTitle: "",
+      content: [
+        "Conceptualized the original project with Kevin Willer"
       ]
     },
     {
       name: "illinois-technology-association",
       title: "Illinois Technology Association",
       content: [
-        "Helped them redefine their relationship with the growing Chicago technology and startup community",
-        "Wall Street Journal mention"
+        "Helped redefine the ITA's relationship with the growing Chicago technology and startup community",
+        "Cited by the Wall Street Journal"
       ]
     },
     {
@@ -315,14 +320,6 @@ export default class {
       title: "Improv Everywhere - No Pants Subway Ride",
       content: [
         "Chicago Tribune - front-page Cover photo and interviewee",
-      ]
-    },
-    {
-      name: "lakes-of-fire",
-      title: "Lakes of Fire - Regional Burn Event",
-      content: [
-        "What Where When form - distrubuted event management",
-        "Provided medical assistance and leadership in a time of crisis, prevented loss of life",
       ]
     },
   ]
